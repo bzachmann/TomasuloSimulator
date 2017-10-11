43b3691d8242ed95eb26cd92eda6399a37dd6bd4
bzachmann <boston.a.zachmann@ndsu.edu>
2017-10-10 20:54:07 -0500

    Added input file and implemented ifstream to read input file

--- qt/tomasulosimulator/main.cpp
+++ qt/tomasulosimulator/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -6,11 +8,24 @@ int main(int argc, char* argv[])
 {
     if(argc < 2)
     {
-        cout << "Usage: " << argv[0] << " <Input File>" <<endl;
+        cout << "Usage: " << argv[0] << " <Input File>" << endl;
     }
     else
     {
-
+        string line;
+        ifstream inputfile (argv[1]);
+        if(inputfile.is_open())
+        {
+            while(getline(inputfile, line))
+            {
+                cout << line << endl;
+            }
+            inputfile.close();
+        }
+        else
+        {
+            cout << "Unable to open input file" << endl;
+        }
     }
 
     return 0;
