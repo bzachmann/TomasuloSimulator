b6c04c255260ffc06cd3f425cd7892275b769c4e
bzachmann <boston.a.zachmann@ndsu.edu>
2017-10-11 21:39:37 -0500

    Completely parses input file and sets up inital IQ and RF

--- qt/tomasulosimulator/main.cpp
+++ qt/tomasulosimulator/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include "processor.h"
 
@@ -11,6 +12,9 @@ RegisterFile initialRF;
 uint16_t numCycles = 0;
 
 bool parseInputFile(char* file);
+bool parseUint16(string line, uint16_t &num);
+bool parseInt32(string line, int32_t &num);
+bool parseInstruction(string line, InstructionRecord &instr);
 
 int main(int argc, char* argv[])
 {
@@ -43,17 +47,92 @@ bool parseInputFile(char* file)
     ifstream inputfile(file);
     if(inputfile.is_open())
     {
-        while(getline(inputfile, line))
+        uint16_t numInstr = 0;
+        if(!getline(inputfile, line)) return false;
+        if(!parseUint16(line, numInstr)) return false;
+
+        if(!getline(inputfile, line)) return false;
+        if(!parseUint16(line, numCycles)) return false;
+
+        InstructionRecord tempInstr;
+        for(uint16_t i = 0; i < numInstr; i++)
         {
-            cout << line << endl;
+            if(!getline(inputfile, line)) return false;
+            if(!parseInstruction(line, tempInstr)) return false;
+            initialIQ.put(tempInstr);
         }
+
+        for(uint16_t i = 0; i < RF_SIZE; i++)
+        {
+            int32_t tempValue = 0;
+            if(!getline(inputfile, line)) return false;
+            if(!parseInt32(line, tempValue)) return false;
+            if(!initialRF.setValue(i,tempValue)) return false;
+        }
+
         inputfile.close();
         retVal = true;
     }
     else
     {
-        cout << "Unable to open input file" << endl;
+        cout << "Unable to locate or open input file" << endl;
     }
     return retVal;
 }
+
+bool parseUint16(string line, uint16_t &num)
+{
+    bool retVal = false;
+    try
+    {
+        num = std::stoi(line);
+        retVal = true;
+    }
+    catch(...)
+    {
+        retVal = false;
+    }
+    return retVal;
+}
+
+bool parseInt32(string line, int32_t &num)
+{
+    bool retVal = false;
+    try
+    {
+        num = std::stoi(line);
+        retVal = true;
+    }
+    catch(...)
+    {
+        retVal = false;
+    }
+    return retVal;
+}
+
+bool parseInstruction(string line, InstructionRecord &instr)
+{
+    std::istringstream iss(line);
+    int32_t tempValue = 0;
+
+    if(!(iss >> tempValue)) return false;
+    if(!((tempValue >= 0) && (tempValue <= 3))) return false;
+    instr.setOpcode((InstructionRecord::opcode_t)tempValue);
+
+    if(!(iss >> tempValue)) return false;
+    if(!((tempValue >= 0) && (tempValue <= RF_SIZE))) return false;
+    instr.setDestination((uint8_t)tempValue);
+
+    if(!(iss >> tempValue)) return false;
+    if(!((tempValue >= 0) && (tempValue <= RF_SIZE))) return false;
+    instr.setSource1((uint8_t)tempValue);
+
+    if(!(iss >> tempValue)) return false;
+    if(!((tempValue >= 0) && (tempValue <= RF_SIZE))) return false;
+    instr.setSource2((uint8_t)tempValue);
+
+    return true;
+}
+
+
 
